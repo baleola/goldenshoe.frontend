58d446cbfd8c11b54076d567e8afb5ae9c3627c9
ola-bale <58167011+ola-bale@users.noreply.github.com>
2020-02-08 08:13:44 +0000

    add link to product images

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Button } from "react-bootstrap";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   return (
     <div>
       <Card style={{ width: "17rem" }}>
-        <Card.Img variant="top" src={product.imgURL} />
+        <Link to={`/products/${product._id}`}>
+          <Card.Img variant="top" src={product.imgURL} />
+        </Link>
         <Card.Body>
           <Card.Title>{product.item}</Card.Title>
           <Card.Text>
